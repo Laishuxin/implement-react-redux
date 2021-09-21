15090df8929e10f70f5a14217089d1392ec6d19e
Laishuxin <56504759+Laishuxin@users.noreply.github.com>
2021-09-22 00:45:07 +0800

    feat(redux): reducer 的由来

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+// @ts-nocheck
+import { useState, useContext, createContext } from 'react'
+
+/**
+ * 基于旧的 `state` 创建新的 `state`，如果不需要改变原有的 `state`，则返回上一次的 `state`。
+ * @param state
+ * @param { type: string, payload: any }
+ * @returns newState
+ */
+const reducer = (state, { type, payload }) => {
+  if (type === 'updateUser') {
+    return {
+      ...state,
+      user: {
+        ...state.user,
+        ...payload,
+      },
+    }
+  }
+  return state
+}
+
+const AppContext = createContext(null)
+const App = () => {
+  const [appState, setAppState] = useState({
+    user: { name: 'frank', age: 18 },
+  })
+
+  const contextValue = { appState, setAppState }
+  return (
+    <AppContext.Provider value={contextValue}>
+      <Parent />
+      <Son />
+      <Grandson />
+    </AppContext.Provider>
+  )
+}
+
+const Parent = () => (
+  <section>
+    <h3>Parent</h3>
+    <User />
+  </section>
+)
+
+const Son = () => (
+  <section>
+    <h3>Son</h3>
+    <UserModifier />
+  </section>
+)
+
+const Grandson = () => (
+  <section>
+    <h3>Grandson</h3>
+  </section>
+)
+
+const User = () => {
+  const contextValue = useContext(AppContext)
+  return <div>user: {contextValue.appState.user.name}</div>
+}
+
+const UserModifier = () => {
+  const { appState, setAppState } = useContext(AppContext)
+  const onChange = e =>
+    setAppState(
+      reducer(appState, {
+        type: 'updateUser',
+        payload: { name: e.target.value },
+      }),
+    )
+
+  return (
+    <div>
+      <input value={appState.user.name} onChange={onChange} />
+    </div>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,5 @@
 // @ts-nocheck
-import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
