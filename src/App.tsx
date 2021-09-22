6dd43eccf2f45191da47cf46e9a410101f7ac8bd
Laishuxin <56504759+Laishuxin@users.noreply.github.com>
2021-09-22 13:59:12 +0800

    feat(redux): redux 雏形

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 // @ts-nocheck
-import { useState, useContext, createContext } from 'react'
+import { useContext, createContext, useState, useEffect } from 'react'
+
+const store = {
+  state: {
+    user: { name: 'frank', age: 18 }, // 暂且进行简单的初始化
+  },
+  setState(newState) {
+    store.state = newState
+    store.listeners.forEach(fn => fn(store.state))
+  },
+  listeners: [],
+  subscribe(fn) {
+    store.listeners.push(fn)
+    return () => {
+      const index = store.listeners.indexOf(fn)
+      if (index >= 0) store.listeners.splice(index, 1)
+    }
+  },
+}
 
 /**
  * 基于旧的 `state` 创建新的 `state`，如果不需要改变原有的 `state`，则返回上一次的 `state`。
@@ -20,15 +38,27 @@ const reducer = (state, { type, payload }) => {
   return state
 }
 
+// 将 createWrapper 改名为 connect
+// 同时，优化内部代码。
+const connect = Component => {
+  return props => {
+    const { state, setState } = store
+    const [, update] = useState({})
+
+    // 别忘了导入 useEffect
+    useEffect(() => store.subscribe(() => update({})), [])
+
+    const dispatch = action => {
+      setState(reducer(state, action))
+    }
+    return <Component {...props} dispatch={dispatch} state={state} />
+  }
+}
+
 const AppContext = createContext(null)
 const App = () => {
-  const [appState, setAppState] = useState({
-    user: { name: 'frank', age: 18 },
-  })
-
-  const contextValue = { appState, setAppState }
   return (
-    <AppContext.Provider value={contextValue}>
+    <AppContext.Provider value={store}>
       <Parent />
       <Son />
       <Grandson />
@@ -36,47 +66,44 @@ const App = () => {
   )
 }
 
-const Parent = () => (
-  <section>
-    <h3>Parent</h3>
-    <User />
-  </section>
-)
-
-const Son = () => (
-  <section>
-    <h3>Son</h3>
-    {/* 将 Wrapper 修改为 UserModifier */}
-    <UserModifier />
-  </section>
-)
-
-const Grandson = () => (
-  <section>
-    <h3>Grandson</h3>
-  </section>
-)
-
-const User = () => {
-  const contextValue = useContext(AppContext)
-  return <div>user: {contextValue.appState.user.name}</div>
+const Parent = () => {
+  console.log('Parent: ', Math.random())
+  return (
+    <section>
+      <h3>Parent</h3>
+      <User />
+    </section>
+  )
 }
 
-// 将 createWrapper 改名为 connect
-// 同时，优化内部代码。
-const connect = Component => {
-  return props => {
-    const { appState, setAppState } = useContext(AppContext)
-    const dispatch = action => {
-      setAppState(reducer(appState, action))
-    }
-    return <Component {...props} dispatch={dispatch} state={appState} />
-  }
+const Son = () => {
+  console.log('Son: ', Math.random())
+  return (
+    <section>
+      <h3>Son</h3>
+      <UserModifier />
+    </section>
+  )
 }
+
+const Grandson = () => {
+  console.log('Grandson: ', Math.random())
+  return (
+    <section>
+      <h3>Grandson</h3>
+    </section>
+  )
+}
+
+const User = connect(({ state }) => {
+  console.log('User: ', Math.random())
+  return <div>user: {state.user.name}</div>
+})
 
 // 将 Wrapper 修改为 UserModifier
 // 同时，将原先的 UserModifier 的代码放在 createWrapper 的参数中。
 const UserModifier = connect(({ dispatch, state }) => {
+  console.log('UserModifier: ', Math.random())
   const onChange = e =>
     dispatch({ type: 'updateUser', payload: { name: e.target.value } })
 
